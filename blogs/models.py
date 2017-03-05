4aa2e0b2ad776588abc6fb7708a98271d44b0fd3
Ahuanghaifeng <1490738477@qq.com>
2017-03-05 22:21:08 +0800

    创建Modle ORM
    添加数据库，查找显示数据

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,8 @@
+from  __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    # 博客标题定义为字符串，最大长度为32默认值是title
+    title = models.CharField(max_length=32, default='Title')
+    # 定义blog的内容，内容可以为空
+    content = models.TextField(null=True)
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from . import models
 
 def index(request):
-    return render(request, 'blog/index.html', {'hello': 'hello,blog!'})
+    # pk 是Primary key主键的意思即为文章id
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'blog/index.html', {'article':article})
 # Create your views here.
