1f0a709bd68d2b73646e053eeaf917262c24d863
Ahuanghaifeng <1490738477@qq.com>
2017-03-05 23:32:13 +0800

    博客主页列表页
    主要是对模板的for循环的用法

--- blogs/views.py
+++ blogs/views.py
@@ -4,6 +4,6 @@ from . import models
 
 def index(request):
     # pk 是Primary key主键的意思即为文章id
-    article = models.Article.objects.get(pk=1)
-    return render(request, 'blog/index.html', {'article':article})
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
 # Create your views here.
