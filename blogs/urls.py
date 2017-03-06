71f729c05e9e7da951f8c127f582c7546487b590
Ahuanghaifeng <1490738477@qq.com>
2017-03-06 21:43:34 +0800

    博客详情页路由加页面
    模型：是以创建的Models,其中包含一个自动生成的ID
    视觉：根据表的ID进行获取数据，article =
    models.Article.object.get(pk=article_id)，并通过render函数将数据返回，html中再通article.title和article.content进行显示。
    控制：urls.py通过正则表达进行限制r'^article/(?P<article_id>[0-9]+)'.P大写，0-9中间不是逗号。

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # ^$约束空字符串 ^index/$分号很重要
     url(r'^$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
 from . import models
 
 def index(request):
     # pk 是Primary key主键的意思即为文章id
     articles = models.Article.objects.all()
     return render(request, 'blog/index.html', {'articles': articles})
-# Create your views here.
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, "blog/article_page.html", {'article': article})
